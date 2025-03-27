b4f59b7d5409be30205ea6eaef3489144df6c798
IIFM-Chennai <iifmcertificates@gmail.com>
2025-03-27 10:50:05 +0530

    manufaly added the sitemap file and update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import Sitemap from "vite-plugin-sitemap";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Sitemap({
-      hostname: "https://iifmglobleverification.web.app", // Your Firebase Hosting URL
-      include: [
-        "/", 
-        "/about", 
-        "/contact", 
-        "/login",
-        "/dashboard",
-        "/dashboard/manage-candidate",
-        "/dashboard/add-candidate",
-        "/dashboard/search-candidate",
-        "/dashboard/candidate/:candidateId"
-      ], // Now including dashboard pages
-
-      // disable automatic robots.txt creation
-      generateRobotsTxt: false,
-    }),
-  ],
-});
+  plugins: [react()],
+})
